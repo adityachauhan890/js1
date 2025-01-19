ba5f77fdae3fe7c437d268ed9c6ec3997db846ed
adityachauhan890 <153818395+adityachauhan890@users.noreply.github.com>
2025-01-20 01:47:20 +0530

    conversion of datatypes

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+/*let score="33"
+console.log(typeof(score))
+let valueInNumber=Number(score)
+console.log(typeof(valueInNumber))*/
+
+/*let score="33abc"
+console.log(typeof score)
+let valueInNumber=Number(score)
+console.log(valueInNumber)*/
+// Number(33abc)=NaN
+
+/*let score=null
+console.log(typeof score)
+let valueInNumber=Number(score)
+console.log(valueInNumber)*/
+// Number(null)=0
+
+/*let score
+console.log(typeof score)
+let valueInNumber=Number(score)
+console.log(valueInNumber)*/
+//number(undefined)=NaN
+
+/*let score="hitesh"
+console.log(typeof score)
+let valueInNumber=Number(score)
+console.log(valueInNumber)*/
+//number("hitesh")=NaN
+
+/*let score=1
+console.log(typeof score)
+let valueInNumber=Boolean(score)
+console.log(valueInNumber)*/
+//Boolean(1)=true; 0=> false
+// ""=> false
+// "adi"=>true
+
+let score=33
+console.log(typeof score)
+let valueInNumber=String(score)
+console.log(valueInNumber)
+console.log(typeof valueInNumber)
