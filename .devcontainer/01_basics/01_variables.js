82c3931bb9fc153c680c775f551e851c6e29d8e2
adityachauhan890 <153818395+adityachauhan890@users.noreply.github.com>
2025-01-20 01:29:15 +0530

    variables of js

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=144552
+let accountemail="adgsh@gmail.com"
+var accountpassword="136553684863"
+accountcity="Jaipur"
+
+//accountId=3
+accountemail="asdf@gmail.com"
+accountpassword="5386387"
+accountcity="Jhajjar"
+let accountstate
+console.log(accountemail)
+console.table([accountId,accountemail,accountpassword,accountcity,accountstate])
+/*prefer not to use var because of issue in scope
+*/
+
+
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"//treat all JS code as newer version
+//alert(3+3) we are using node JS not browser
+// code readability should be high
+let name="Aditya"
+let age=20
+let isloggedin= false
+let state=null
+// number => 2 to the power 53
+//bigint
+// string=>""
+//boolean=> true/false
+//null => standalone value
+//undefined => value not assigned
+// symbol => unique
+// object 
+console.log(typeof"adi")
+console.log(typeof(age));
+console.log(typeof null )
+
