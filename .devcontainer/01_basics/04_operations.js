98fc5395a22251c009795aa6d4988aa417582558
adityachauhan890 <153818395+adityachauhan890@users.noreply.github.com>
2025-01-20 13:59:54 +0530

    clearing confusion related to operations

---
+++ .devcontainer/01_basics/04_operations.js
@@ -0,0 +1,31 @@
+/*let value=3
+let negvalue= -value
+console.log(negvalue)
+console.log(2+2)
+console.log(2-9)
+console.log(2**9)
+console.log(9%2)
+console.log(9/2)*/
+
+/*console.log(2+"2");
+console.log("2"+2);
+console.log("2"+2+2);
+console.log(2+2+"2");*/
+
+/*console.log("2">=2);//gives true
+console.log("2">2); //gives false
+console.log("2">1);*/ //gives true
+// but comparing different data types can not always be handy
+
+/*console.log(null > 0); //gives false
+console.log(null == 0); //gives false
+console.log(null >= 0);*/ // gives true 
+
+console.log(undefined==0);
+
+// === strict check this even compares the data type
+console.log("2"===2);
+console.log(2===2);
+
+
+
