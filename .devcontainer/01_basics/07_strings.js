699ab1352d9cd6423658926b6292e822fa874e3c
adityachauhan890 <153818395+adityachauhan890@users.noreply.github.com>
2025-01-25 01:37:04 +0530

    Update 07_strings.js

---
+++ .devcontainer/01_basics/07_strings.js
@@ -0,0 +1,11 @@
+/*const name="aditya"
+const age=18
+const adress="delhi"
+console.log(name+age+adress+"frank@gmail.com");
+// outdated 
+
+console.log(`Hello my name is ${name} and my age is ${age}`)*/
+const str="        fgh          "
+console.log(str);
+console.log(str.trim());
+
